b3c854a8b59fd1f606dc911c92b969d835ecbca4
Lele Shu <lele.shu@gmail.com>
2020-01-21 16:03:25 -0800

    Add the CloseBoundray calculation into Element Surface/Subsurface flux computations.

--- src/ModelData/MD_ElementFlux.cpp
+++ src/ModelData/MD_ElementFlux.cpp
@@ -11,12 +11,13 @@ void Model_Data::fun_Ele_Infiltraion(int i, double t){
 }
 void Model_Data::fun_Ele_surface(int i, double t){
     int j, inabr;
-    double  Ymean, dh, s, CrossA, Q;
+    double  Ymean, dh, s, CrossA, Q, B;
     double isf, nsf; // Available Y in Surface of this/nabor element    
     isf = uYsf[i] - qEleInfil[i] + qEleExfil[i];
     isf = isf < 0. ? 0. : isf;
     for (j = 0; j < 3; j++) {
         inabr = Ele[i].nabr[j] - 1;
+        B = Ele[i].edge[j];
         if (inabr >= 0) {
             /***************************************************************************/
             /* Surface Lateral Flux Calculation between Triangular elements Follows */
@@ -31,7 +32,7 @@ void Model_Data::fun_Ele_surface(int i, double t){
                 Q = 0.;
             }else{
                 s = dh / Ele[i].Dist2Nabor[j];
-                CrossA = Ymean * Ele[i].edge[j];
+                CrossA = Ymean * B;
                 if(s > 0 && isf <=0){
                     Q = 0.;
                 }else if(s < 0 && nsf <=0){
@@ -42,7 +43,17 @@ void Model_Data::fun_Ele_surface(int i, double t){
 //                CheckNANi(QeleSurf[i][j], i, "QeleSurf[i][j]");
             } //end of ifelse Avg_Y_Surf < 0.
         } else {
-            Q= 0;
+            Q = 0;
+            if(CS.CloseBoundary){
+                /* Void */
+            }else{
+                if(isf > Ele[i].depression){
+                    s = isf / Ele[i].Dist2Edge[j] * 0.5;
+                    if(s > 0.){
+                        Q = sqrt(s) * cbrt(isf * isf * isf * isf * isf) * B / Ele[i].Rough;
+                    }
+                }
+            }
         } // end of if
         QeleSurf[i][j] = Q;
     } // end of for loop
@@ -74,6 +85,16 @@ void Model_Data::fun_Ele_sub(int i, double t){
             }
         } else {
             Q = 0;
+            if(CS.CloseBoundary){
+                /* Void */
+            }else{
+                if(uYgw[i] > Ele[i].depression * 10.){
+                    grad = uYgw[i]  / Ele[i].Dist2Edge[j] * 0.5;
+                    if(grad > 0.){
+                        Q = Ele[i].u_effKH * grad;
+                    }
+                }
+            }
         } // end of if
         QeleSub[i][j] = Q;
     } // end of for loop
