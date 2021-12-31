a6618b025ef75c4a8dbb1c5a11b806c67e7b136e
Lele Shu <lele.shu@gmail.com>
2021-12-31 11:16:48 +0800

    UPDATE: forcing units.

--- src/ModelData/MD_CheckInputData.cpp
+++ src/ModelData/MD_CheckInputData.cpp
@@ -45,19 +45,21 @@ void Model_Data::CheckInput_forc(){
     /********************************************
     This function cannot find the abnormal values,
      but find possible error from WRONG UNIT,
-     e.g. precipitation in mm/day, mm/hr;
-     Temperature in Kelvin instead of Celcius
+     e.g. precipitation in m/day[RIGHT],
+        rather than mm/day[WRONG], mm/hr[WRONG];
+     Temperature in Kelvin[WORNG]
+        instead of Celcius[RIGHT]
      *******************************************/
     for(int i = 0; i < NumForc; i++){
-        tsd_weather[i].checkValue(i_prcp, 0., 0.4 * 24, "Prcp");
+        tsd_weather[i].checkValue(i_prcp, 0., 300, "Prcp"); /* 300 mm/day is hurricane level*/
         /*******************************************
          Maximum hourly precipitation. 0.4 m/hr.
          ref: http://www.nws.noaa.gov/oh/hdsc/record_precip/record_precip_world.html
          ********************************************/
         tsd_weather[i].checkValue(i_temp, -70., 50., "Temp");
-        tsd_weather[i].checkValue(i_rh, 0., 1.5, "RH"); /* Theoretical MAX rh is 1 (100%). */
-        tsd_weather[i].checkValue(i_wind, 0., 50. * 86400., "Wind"); /*50m/s is 13-level Typhoon. */
-        tsd_weather[i].checkValue(i_rn, 0., 1360. * 86400, "Radiation");
+        tsd_weather[i].checkValue(i_rh, 0., 120, "RH"); /* Theoretical MAX rh is 100 (100%). */
+        tsd_weather[i].checkValue(i_wind, 0., 50. , "Wind"); /*50m/s is 13-level Typhoon. */
+        tsd_weather[i].checkValue(i_rn, 0., 1360. , "Radiation"); /* Terrestrial radiation = 1360. */
         /* max = solar constant. 1360 */
     }
 }
